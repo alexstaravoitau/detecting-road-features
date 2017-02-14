9468313d414075d3d456dcb5c4b6986424b84abf
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-02-14 23:50:11 +0000

    Add routines for perspective transforms.

---
+++ lanetracker/perspective.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+
+
+def flatten_perspective(image):
+    # Get image dimensions
+    (h, w) = (image.shape[0], image.shape[1])
+    # Define source points
+    source = np.float32([[564, 450], [716, 450], [-100, 720], [1380, 720]])
+    # Define corresponding destination points
+    destination = np.float32([[100, 0], [w - 100, 0], [100, h], [w - 100, h]])
+    transform_matrix = cv2.getPerspectiveTransform(source, destination)
+    unwarp_matrix = cv2.getPerspectiveTransform(destination, source)
+    return (cv2.warpPerspective(image, transform_matrix, (w, h)), unwarp_matrix)
