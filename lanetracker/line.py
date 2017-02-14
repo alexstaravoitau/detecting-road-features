f93551748febbc9c91438643945f66229f71e714
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-02-14 23:59:52 +0000

    Update reference values.

--- lanetracker/line.py
+++ lanetracker/line.py
@@ -21,7 +21,7 @@ class Line(object):
 
     def radius_of_curvature(self):
         # Define conversions in x and y from pixels space to meters
-        ym_per_pix = 24 / 720  # meters per pixel in y dimension
+        ym_per_pix = 27 / 720  # meters per pixel in y dimension
         xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
         # Fit new polynomials to x,y in world space
         y = self.points[:, 1]
