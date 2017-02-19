c09f45fbf5c9c78fb74c1e69d1a544db62ffce90
Alex Staravoitau <alex.staravoitau@gmail.com>
2017-02-19 21:01:58 +0000

    Add values initialisation and docstring comments.

--- lanetracker/window.py
+++ lanetracker/window.py
@@ -19,19 +19,20 @@ class Window(object):
         tolerance   : Min number of pixels we need to detect within a window in order to adjust its x coordinate.
         """
         self.x = x
+        self.mean_x = x
         self.y1 = y1
         self.y2 = y2
         self.m = m
         self.tolerance = tolerance
 
-    def pixels_in(self, nonzero):
+    def pixels_in(self, nonzero, x=None):
         """
         Returns indices of the pixels in `nonzero` that are located within this window.
 
         Notes
         -----
         Since this looks a bit tricky, I will go into a bit more detail. `nonzero` contains two arrays of coordinates
-        non-zero pixels. Say, there were 50 non-zero pixels in the image and `nonzero` would contain two arrays of
+        of non-zero pixels. Say, there were 50 non-zero pixels in the image and `nonzero` would contain two arrays of
         shape (50, ) with x and y coordinates of those pixels respectively. What we return here is a array of indices
         within those 50 that are located inside this window. Basically the result would be a 1-dimensional array of
         ints in the [0, 49] range with a size of less than 50.
@@ -58,4 +59,11 @@ class Window(object):
         return win_indices
 
     def coordinates(self):
+        """
+        Returns coordinates of the bounding rect.
+
+        Returns
+        -------
+        Tuple of ((x1, y1), (x2, y2))
+        """
         return ((self.x - self.m, self.y1), (self.x + self.m, self.y2))
